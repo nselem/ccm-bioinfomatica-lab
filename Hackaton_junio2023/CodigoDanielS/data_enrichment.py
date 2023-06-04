3b98fe51f5686ddd24f88c2791abcb83a6e8636c
DanielS <daniel@amphorahealth.com>
2023-06-04 12:18:07 -0600

    feat: :sparkles: data merge into one table (2016 and 2017)

--- Hackaton_junio2023/CodigoDanielS/data_enrichment.py
+++ Hackaton_junio2023/CodigoDanielS/data_enrichment.py
@@ -14,12 +14,33 @@ from sklearn.preprocessing import (Binarizer, LabelEncoder, MinMaxScaler,
                                    PowerTransformer, QuantileTransformer,
                                    RobustScaler, StandardScaler)
 
+
+# Scalers to use
+scalers = {
+  "MinMaxScaler": MinMaxScaler,
+  "Normalizer": Normalizer,
+  "PowerTransformer": PowerTransformer,
+  "QuantileTransformer": QuantileTransformer,
+  "RobustScaler": RobustScaler,
+  "StandardScaler": StandardScaler,
+}
+
 # Import data
-df_i = pd.read_csv('Hackaton_junio2023/CodigoDanielS/c23/biom_2016.tsv', sep='\t', header=0)
+df_1 = pd.read_csv('Hackaton_junio2023/CodigoDanielS/c23/biom_2016.tsv', sep='\t', header=0)
+df_2 = pd.read_csv('Hackaton_junio2023/CodigoDanielS/c23/biom_2017.tsv', sep='\t', header=0)
+
+#rename first column 'OTU'->'OTU ID'
+df_2.rename(columns={'OTU':'OTU ID'}, inplace=True)
+
+# Merge data
+df_i = pd.merge(df_1, df_2, on='OTU ID', how='outer')
+
+# NaN values are replaced by 0
+df_i.fillna(0, inplace=True)
 
 # Block to enrich the data -----------------------------------------------------
 data_x = np.array(df_i.iloc[:,1:], dtype=float).T
-data_y = [h.split('_')[-2] for h in df_i.head() if h.startswith("CAMDA")]
+data_y = ["_".join(h.split('_')[-3:-1]) for h in df_i.head() if h.startswith("CAMDA")]
 
 # Prepare the SMOTE algorithm
 smote = SMOTE(sampling_strategy='auto', k_neighbors=5, random_state=42)
@@ -36,37 +57,34 @@ for i in range(len(data_y)):
   df_e[f"SMOTE_MetaSUB_gCSD16_{city}_{idx[city]:02d}"] = data_x[i]
 
 # -----------------------------------------------------------------------------
-def normalize_data(_df,prefix=""):
+def normalize_data(_df,prefix="",norm="MinMaxScaler"):
   # # take a sabple of the data with N columns and N rows
   # N = 10
   # df_i = df_i.iloc[:N,:N]
 
   # extract data from the database
   data = _df.iloc[:,1:].values
-  data = np.array(data, dtype=float).T
+  data = np.array(data, dtype=float)
 
   # Start the normalization process
-  scaler = QuantileTransformer()
+  scaler = scalers[norm]()
   df_norm = scaler.fit_transform(data)
 
   # Save into the original dataframe
   df_o = _df.copy()
-  df_o.iloc[:,1:] = df_norm.T
+  df_o.iloc[:,1:] = df_norm
+
+  # Add new rows to the dataframe with the year of the sample (0:2016, 1:2017)
+  new_row = pd.Series([-1]+[0 if "gCSD16" in h else 1 for h in df_o.columns[1:]], index=df_o.columns)
+  df_o = pd.concat( [new_row.to_frame().T, df_o], ignore_index=True)
 
   # Block of prints manhattan like plot -----------------------------------------
-  city1 = df_o.iloc[1:,1]
-  city2 = df_o.iloc[1:,2]
-  city3 = df_o.iloc[1:,3]
-  plt.scatter(range(len(city1)), city1, label='city1', s=1)
-  plt.scatter(range(len(city2)), city2, label='city2', s=1)
-  plt.scatter(range(len(city3)), city3, label='city3', s=1)
-  cityb1 = df_o.iloc[1:,-1]
-  cityb2 = df_o.iloc[1:,-2]
-  cityb3 = df_o.iloc[1:,-3]
-  plt.scatter(range(len(cityb1)), cityb1, label='cityb1', s=1)
-  plt.scatter(range(len(cityb2)), cityb2, label='cityb2', s=1)
-  plt.scatter(range(len(cityb3)), cityb3, label='cityb3', s=1)
-  plt.legend()
+  # heatmap of the data as a meshgrid
+  plt.figure(figsize=(16,9))
+  plt.pcolormesh(df_norm.T, cmap='viridis')
+  plt.colorbar()
+  plt.xlabel("Otus/derived data")
+  plt.ylabel("Samples")
 
   # Block to save the data into a file ------------------------------------------
   scaler_name = scaler.__class__.__name__
@@ -78,8 +96,10 @@ def normalize_data(_df,prefix=""):
 
 
 if __name__ == "__main__":
-  # normalize input data
-  normalize_data(df_i, prefix="original")
-  # normalize enrichment data
-  normalize_data(df_e, prefix="enriched")
+  sc_list = list(scalers.keys())
+  for sc in sc_list:
+    # normalize input data
+    normalize_data(df_i, prefix="original", norm=sc)
+    # normalize enrichment data
+    normalize_data(df_e, prefix="enriched", norm=sc)
   pass
